bac2bb0f894a0e26819881a74095b844353a5d8a
Marc Rodriguez <marcrodrimartin@gmail.com>
2024-08-14 12:31:37 +0200

    Update our story constants.

--- config/constants.php
+++ config/constants.php
@@ -6,27 +6,109 @@ return [
         "ale" => [
             "title" => "La historia de Alexandra",
             "text" => "¡Hola a todos!
-                    Soy Ale, y tengo una confesión: ¡mi vida está llena de pequeñas obsesiones que hacen que cada día sea emocionante! Primero, soy medio alemana, así que mi amor por la Navidad es digno de un villancico. ¡Para mí, diciembre no es suficiente! ¡Que nieve en julio y cantidades industriales de chocolate caliente también, por favor! La comida italiana: pizza y pasta son mis grandes amores culinarios.
-                    Además de mi amor por el frío y la Navidad, disfruto mucho del deporte. El fútbol y las carreras de 10k ocupan la mayoría de mis tardes. ¡Nada como una buena carrera para mantenerme en forma y sentir la adrenalina!
-                    Una buena conversación acompañada de un café es uno de mis placeres favoritos. Ponme algo de música española de los 2000, y no podré resistirme a bailar.
-                    Soy muy católica, y asistir a misa el sábado por la tarde es mi plan favorito de la semana. También soy una gran lectora; no hay nada como perderse en un buen libro para desconectar del mundo. Por cierto, uno de mis poetas favoritos es Mario Benedetti. Sus versos siempre tienen un lugar especial en mi corazón.
-                    No puedo olvidar a mi fiel amigo Capo, que me acompaña a todos lados, no podría vivir sin él.
-                    Y, no os voy a engañar, tengo un gran sueño desde pequeña: vestirme de blanco. Estoy muy emocionada por ese día y todo lo que vendrá con él.
-                    Así que, en resumen, soy una mezcla de nieve en verano, chocolate caliente, pizza italiana, fe amor por la vida, cafés con amigos, un buen libro, y el sueño de tener una familia numerosa."
+                        Vengo a contaros algo: ¡mi vida está llena de pequeñas ilusiones que hacen que cada día
+                        sea emocionante! Primero, soy medio alemana, así que mi amor por la Navidad es digno
+                        de un villancico. Para mí, diciembre no es suficiente. Soy de nieve en julio y de
+                        cantidades gigantes de chocolate caliente los 365 días del año.
+                        Tengo una lista de pasiones que es casi tan extensa como mi amor por la Navidad. Me
+                        encanta comer platos italianos: la pizza y la pasta son mis grandes amores culinarios
+                        (¿quién puede resistirse a la lasaña de mi madre?), hacer deporte (nada como una buena
+                        carrera con amigos para mantenerme en forma y sentir la adrenalina al llegar a la meta)
+                        viajar (porque el mundo es demasiado grande para quedarse en un solo lugar) y hacer
+                        punto de cruz con mi abuela (alguien me dijo una vez que parece que tenga entre 30 y
+                        90 años).
+                        Una buena conversación acompañada de un buen café es uno de mis placeres favoritos y
+                        ni te cuento lo feliz que soy si fuera llueve. Ponme “Corazón latino” de David Bisbal, y
+                        no podré resistirme a bailar y a cantar a pleno pulmón.
+                        Soy muy católica y asistir a misa el sábado por la tarde es uno de mis planes favoritos
+                        de la semana. También soy una gran lectora; no hay nada como perderse en un buen
+                        libro para desconectar del mundo. Por cierto, uno de mis poetas favoritos es Mario
+                        Benedetti.
+                        No podría vivir sin Capo, estamos juntos 24/7. Y, no os voy a engañar, tengo un gran
+                        sueño desde pequeña: vestirme de blanco y tener una familia numerosa. Estoy muy
+                        emocionada por el gran día, por casarme con alguien como Carlos y todo lo que vendrá
+                        con él.
+                        Y, por supuesto debo decir que nada de esto sería lo mismo sin mi familia y sin mis
+                        amigos. Ellos son mi equipo de aventuras, mi banda sonora y los que hacen que cada
+                        día sea una fiesta.
+                        Así que, en resumen, soy una mezcla de montañas nevadas en julio, chocolate caliente y
+                        un plato doble de espaguetis, un entusiasmo infinito por la vida, el deporte, la comida y,
+                        por supuesto, la buena compañía."
         ],
         "carlos" => [
             "title" => "La historia de Carlos",
-            "text" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sed purus vitae mi euismod
-                            pretium et vitae ipsum. Integer a tellus imperdiet, dignissim justo a, hendrerit nulla. Praesent
-                            ut vulputate nibh. Morbi porttitor facilisis ligula vitae finibus. Nunc varius non risus vitae
-                            aliquet. Quisque vehicula dictum ornare. Suspendisse"
+            "text" => "¡Ahora mi parte!
+                        Me considero una persona sencilla, que es capaz de disfrutar de los pequeños placeres
+                        del día a día. Sin duda, soy un disfrutón profesional de las escapadas (especialmente si
+                        van acompañadas de buena gastronomía, montaña y desconexión). Me encanta
+                        descubrir nuevos rincones del planeta, conocer nuevas culturas y pasar tiempo con los
+                        míos. Sin duda, no hay mejor forma para mí de cargar pilas.
+                        En el trabajo, soy un poco “friki” del derecho, nos os voy a engañar. Guardo como una
+                        reliquia el código civil de mis primeras oposiciones y, estudio a diario para lograr mi
+                        plaza de funcionario público como si estuviera preparándome para una maratón. Una
+                        maratón que, por cierto, también quiero correr en 2026. Sin duda, otra de mis grandes
+                        pasiones es el deporte. Y, si ese deporte va acompañado con objetivos marcados (media
+                        maratón de Pamplona, media maratón de Barcelona, medio triatlón etc.), mejor que
+                        mejor (no llegaré al nivel necesario para los JJOO de Los Ángeles, pero correr un
+                        triatlón olímpico en mi ciudad, es otra de las grandes ilusiones deportivas que tengo).
+                        Por supuesto, siempre estoy al tanto de la actualidad política, sin duda, otro de mis
+                        pasatiempos favoritos. Y, en materia de lectura os confesaré una cosa. En una de
+                        nuestras primeras citas le propuse a Ale una tertulia literaria. Y, llegará el día de la boda
+                        y, la realidad, es que aún no he leído el libro sobre el que debía versar dicha tertulia. En
+                        mi descargo, solo diré que soy más de “República” de Platón o de “Política” de
+                        Aristóteles, que de leer novela y que “Patria”, de Fernando Aramburu, sigue esperando
+                        su ocasión para esa tertulia. ¡Llegará!
+                        Nuestro piso, es mi refugio en un día a día bastante ajetreado. Por eso, esos primeros 45
+                        minutos del día, a las 6:00 de la mañana, con el sol saliendo, el mundo en silencio,
+                        paseando a Capo primero y tomando un buen café después con la radio de fondo, son
+                        una suerte de ritual para mí.
+                        Y que deciros de la persona que pronto será mi mujer y la persona con la que quiero
+                        formar nuestra familia. Es corazón, alegría, bondad y un torbellino de energía. Por eso,
+                        solo puedo darle gracias a la vida por haberla puesto en mi camino y poderle dar el sí
+                        quiero en muy pocos meses. Una familia, en la que ya hay un miembro muy especial,
+                        Capo. Con él, he entendido que el amor por los animales existe y que no es posible
+                        entenderlo completamente hasta que no se comparte la vida con uno de ellos. ¡Qué
+                        suerte la nuestra contigo, Capo!
+                        Por supuesto, no puedo olvidarme de mi familia y amigos. Gracias por estar siempre ahí
+                        y por haber ayudado a construir, entre todos, la persona en la que me he convertido hoy.
+                        Por cierto, y, aunque muchos ya lo habréis visto, mi look ha evolucionado. Llevo un
+                        moño que ya es mi marca personal. ¡Y sí, confirmo que planeo casarme con él!
+                        ¡Os esperamos a todos, con mucha ilusión, el 30 de noviembre!"
         ],
         "relacion" => [
             "title" => "Nuestra relación",
-            "text" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sed purus vitae mi euismod
-                            pretium et vitae ipsum. Integer a tellus imperdiet, dignissim justo a, hendrerit nulla. Praesent
-                            ut vulputate nibh. Morbi porttitor facilisis ligula vitae finibus. Nunc varius non risus vitae
-                            aliquet. Quisque vehicula dictum ornare. Suspendisse"
+            "text" => "Nos conocimos en un gran despacho de abogados, Cuatrecasas, unidos por nuestra
+                        pasión por el derecho. Entre tantas horas encerrados en aquel edificio, el amor, surgió de
+                        manera inesperada. Desde el primer momento, el flechazo fue instantáneo, aunque (Ale)
+                        “debo confesar que, por aquel entonces, Carlos llevaba el pelo liso (¡y eso no me
+                        gustaba nada!)”. Nuestra conexión, fue tan fuerte que rápidamente descubrimos que
+                        compartíamos muchas más cosas que únicamente nuestra profesión.
+                        Hace tres años, decidimos dar un gran paso y comenzar a vivir juntos. No podemos
+                        estar más contentos con el hecho de haber escogido Barcelona como nuestro hogar. Nos
+                        encanta ir al teatro, al cine, explorar nuevos restaurantes y hacer un running hasta
+                        Montjuic para acabar con la Ciudad Condal debajo de nuestros pies. Pasear o salir en
+                        bici por nuestro querido pueblo, el Canós, con Capo, es otro de nuestros placeres
+                        favoritos. Nuestros domingos tienen una tradición especial: siempre desayunamos
+                        tortitas con forma de corazón.
+                        Además, somos grandes viajeros. Hemos tenido la suerte de explorar destinos increíbles
+                        como Filipinas, Menorca, Albania, Montenegro, Suiza, Francia, Mallorca y Alemania.
+                        Cada viaje nos ha dejado recuerdos inolvidables y nos ha permitido descubrir nuevas
+                        culturas y experiencias juntos.
+                        Nos encanta tener gente en casa y compartir la vida con las personas que queremos. La
+                        música forma parte de nuestras vidas, y nada nos alegra más que cantar a pleno pulmón
+                        “Uno más uno, son siete” mientras estamos en el coche, por eso fijaros bien el día de
+                        nuestra boda, en el vino tinto que hemos escogido. Y un detalle divertido: la serie Los
+                        Serrano siempre está presente en nuestra vida. Ya sea en una conversación casual o en
+                        una maratón de episodios, es uno de esos elementos que nos une y nos hace reír.
+                        Os preguntareis como habrá sido la pedida de mano que nos ha llevado hasta aquí:
+                        (Carlos) ocurrió en Menorca, un lugar muy especial para Ale. Busqué el escenario
+                        perfecto: estábamos en una playa, viendo el amanecer, y fue el momento ideal para
+                        proponerle a Ale compartir la vida juntos. (Fun fact: Ale se tuvo que tomar una pastilla
+                        relajante después de la pedida, no podía dejar de llorar).
+                        Así que, en resumen, nuestra relación es una combinación de amor por el derecho,
+                        exploraciones gastronómicas, paseos con Capo, debates animados, una pasión por los
+                        viajes, cenas con amigos y una pedida de mano inolvidable en Menorca, y una buena
+                        dosis de música y humor."
         ]
     ],
 ];
