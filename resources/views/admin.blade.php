ea95ec25ca14581ff8742f375fbaf87fade52671
Marc Rodriguez <marcrodrimartin@gmail.com>
2024-09-21 13:21:22 +0200

    Fix table size on admin view.

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -30,51 +30,46 @@
             </ul>
         </nav>
         <main>
-
-
-            <div class="relative overflow-x-auto shadow-md sm:rounded-lg mt-10">
-                <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+            <div class="relative overflow-x-auto shadow-md sm:rounded-lg mt-10 flex justify-center">
+                <table class="text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400 w-11/12">
                     <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                         <tr>
-                            <th scope="col" class="text-center px-6 py-3">
+                            <th scope="col" class="text-center px-6 py-3 w-1/7">
                                 Nombre
                             </th>
-                            <th scope="col" class="text-center px-6 py-3">
+                            <th scope="col" class="text-center px-6 py-3 w-1/7">
                                 Apellidos
                             </th>
-                            <th scope="col" class="text-center px-6 py-3">
+                            <th scope="col" class="text-center px-6 py-3 w-1/7">
                                 ¿Transporte al banquete?
                             </th>
-                            <th scope="col" class="text-center px-6 py-3">
+                            <th scope="col" class="text-center px-6 py-3 w-1/7">
                                 ¿Transporte a Casa?
                             </th>
-                            <th scope="col" class="text-center px-6 py-3">
+                            <th scope="col" class="text-center px-6 py-3 w-1/7">
                                 Menú
                             </th>
-                            <th scope="col" class="text-center px-6 py-3">
+                            <th scope="col" class="text-center px-6 py-3 w-1/7">
                                 Alergias
                             </th>
-                            <th scope="col" class="text-center px-6 py-3">
+                            <th scope="col" class="text-center px-6 py-3 w-1/7">
                                 Canción
                             </th>
                         </tr>
                     </thead>
                     <tbody>
                         @foreach ($guestsInfo as $info)
-                            <tr
-                                class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+                            <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
                                 @foreach ($info->only('name', 'surname', 'banquet', 'home', 'menu', 'allergies', 'song') as $key => $value)
                                     @switch($key)
                                         @case('banquet')
-                                            <th scope="row"
-                                                class="text-center px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                            <th scope="row" class="text-center px-6 py-4 font-medium text-gray-900 break-words dark:text-white w-1/7">
                                                 {{ $value == 0 ? 'No' : 'Sí' }}
                                             </th>
                                         @break
-
+        
                                         @default
-                                            <th scope="row"
-                                                class="text-center px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                            <th scope="row" class="text-center px-6 py-4 font-medium text-gray-900 break-words dark:text-white w-1/7">
                                                 {{ $value == '' ? '-' : $value }}
                                             </th>
                                     @endswitch
@@ -82,11 +77,10 @@
                             </tr>
                         @endforeach
                     </tbody>
-
                 </table>
             </div>
-
         </main>
+        
     </div>
 </body>
 
